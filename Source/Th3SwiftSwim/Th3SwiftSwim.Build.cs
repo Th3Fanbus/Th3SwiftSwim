695762120f3863561cb43eed19b880883277ced4
Angel Pons <th3fanbus@gmail.com>
2024-10-16 10:56:54 +0200

    Th3SwiftSwim version 1.1.0
    Update to FG 1.0
    
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>

--- Source/Th3SwiftSwim/Th3SwiftSwim.Build.cs
+++ Source/Th3SwiftSwim/Th3SwiftSwim.Build.cs
@@ -1,36 +1,24 @@
 /* SPDX-License-Identifier: MPL-2.0 */
 
 using UnrealBuildTool;
-using System.IO;
-using System;
 
 public class Th3SwiftSwim : ModuleRules
 {
 	public Th3SwiftSwim(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		bUseUnity = false;
+    {
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        ShadowVariableWarningLevel = WarningLevel.Error;
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        bLegacyParentIncludePaths = false;
+        CppStandard = CppStandardVersion.Cpp20;
+        bUseUnity = false;
 
-		PublicDependencyModuleNames.AddRange(new string[] {
-			"Core", "CoreUObject", "Engine",
-			"DeveloperSettings",
-			"PhysicsCore",
-			"InputCore",
-			"GeometryCollectionEngine",
-			"AnimGraphRuntime",
-			"AssetRegistry",
-			"NavigationSystem",
-			"AIModule",
-			"GameplayTasks",
-			"SlateCore", "Slate", "UMG",
-			"RenderCore",
-			"CinematicCamera",
-			"Foliage",
-			"NetCore",
-			"GameplayTags",
-			"Json", "JsonUtilities",
-			"DummyHeaders",
-			"FactoryGame", "SML"
-		});
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core", "CoreUObject", "Engine",
+            "DeveloperSettings", "PhysicsCore", "InputCore",
+            "AssetRegistry", "RenderCore", "RHI",
+            "SlateCore", "Slate", "UMG", "GameplayTags",
+            "DummyHeaders", "FactoryGame", "SML",
+        });
 	}
 }
